2e790275ab69aebc70b5d080fb1e624aa11cb62c
doris <doris@OTVALI>
2024-01-10 13:00:58 +0200

    Add project files.

---
+++ vigade parandus/vigade_parandus.py
@@ -0,0 +1,40 @@
+﻿#valesti kirjas
+import math
+print("Ruudu karakteristikud")
+#float
+a=float(input('Sisesta ruudu külje pikkus => '))
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+#"
+print("Ruudu ümbermõõt", P)
+#sqrt
+di=a*math.sqrt(2)
+print("Ruudu diagonaal", round(di,2))
+print()
+# 1)
+print("Ristküliku karakteristikud")
+#float
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=float(input("Sisesta ristküliku 2. külje pikkus => "))
+S=b*c
+#""
+print("Ristküliku pindala", S)
+#*
+P=2*(b+c)
+print("Ristküliku ümbermõõt", P)
+# kvadrat**
+di=math.sqrt(b**2+c**2)
+print("Ristküliku diagonaal", round(di,2))
+print()
+print("Ringi karakteristikud")
+#float ""
+r=float(input("Sisesta ringi raadiusi pikkus => "))
+#*
+d=2*r
+print("Ringi läbimõõt", d)
+#math.pi
+S=math.pi*r**2
+print("Ringi pindala", round(S,2))
+C=2*math.pi*r
+print("Ringjoone pikkus", round(C,2))
